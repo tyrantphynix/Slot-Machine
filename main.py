6d639a70e566e3bbd6acc0f718e09003395edf10
tyrantphynix <chirag.rawat1000@gmail.com>
2025-02-28 13:28:30 +0530

    Added spinning and printing of slot machine

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 1000
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_counts = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        all_symbols += [symbol] * symbol_count
+
+    columns = []
+    for _ in range(cols):
+        current_symbols = all_symbols[:]
+        column = []
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+        
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row])
+    
+    
+
 
 def deposit():
     while True:
@@ -47,8 +88,16 @@ def get_bet():
 def main():
     balance = deposit()
     lines = number_of_lines()
-    bet = get_bet() 
-    bet *= lines
-    print(balance, lines, bet)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if balance >= total_bet:
+            break
+        else:
+            print(f"you do not have enough money, Current balance is ₹{balance}"), 
+    print(f"Your balance is ₹{balance}. You bet ₹{bet} on {lines} lines. Total bet is ₹{total_bet}.")
+    columns = get_slot_machine_spin(ROWS, COLS, symbol_counts)
+    print(columns)
+    slots = print_slot_machine(columns)
 
 main()
